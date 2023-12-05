095c8473517d6e09bd7c59fc931e6030072535e6
super <super@DESKTOP-10MPALR>
2023-12-05 16:35:36 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, {useState, useRef} from "react";
 import './App.css';
+import { Auth } from './components/Auth';
+import {Chat} from './components/Chat';
+import {signOut} from 'firebase/auth';
+import {auth} from './firebase-config';
+
+import Cookies from 'universal-cookie'
+const cookies = new Cookies();
+
 
 function App() {
+
+  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
+  const [room, setRoom] = useState(null);
+
+  const roomInputRef = useRef(null);
+  const signUserOut = async () => {
+    await signOut(auth);
+    cookies.remove("auth-token");
+    setIsAuth(false);
+    setRoom(null);
+  }
+
+  const backButtonClick = () => {
+    setRoom(null);
+  }
+
+  if (!isAuth) {
+    return (
+      <div>
+      <Auth setIsAuth={setIsAuth} />
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     <div>
+      {room ? <div> <Chat room={room}/>   <button onClick={backButtonClick} className="back-button">Back</button> </div> : <div className="room"> 
+      <label>Enter Room Name:</label>
+      <input className="new-message-input" ref={roomInputRef} placeholder="Room Name"></input>
+      <button className="button" onClick={() => setRoom(roomInputRef.current.value)}>Enter Chat</button>
+      </div>} 
+    
+    
+
+     <div className="sign-out">
+      <button className="button" onClick={signUserOut}>Sign Out</button>
+     </div>
+
+     </div>
+
+  )
+
+
 }
 
 export default App;
---
+++ src/components/Chat.js
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+import { addDoc, collection, onSnapshot, orderBy, query, serverTimestamp, where } from'firebase/firestore';
+import {auth, db} from "../firebase-config";
+import '../styles/Chat.css';
+import {setRoom} from '../App';
+
+export const Chat = (props) => {
+
+    const {room} = props;
+
+    const [newMessage, setNewMessage] = useState("");
+    const [messages, setMessages] = useState([]);
+
+    const messagesRef = collection(db, "messages");
+
+    useEffect(() => {
+
+        const queryMessages = query(messagesRef, where("room","==", room), orderBy("createdAt"))
+        const unsubscribe = onSnapshot(queryMessages, (snapshot) => {
+            let messages = [];
+           snapshot.forEach((doc) => {
+            messages.push(
+                {   ...doc.data(),
+                    id: doc.id
+                }
+                );
+           })
+        setMessages(messages);
+        })
+        return () => unsubscribe();
+    }, [])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (newMessage === "") return;
+
+        await addDoc(messagesRef, {
+            text: newMessage,
+            createdAt: serverTimestamp(),
+            user: auth.currentUser.displayName,
+            room,
+        });
+
+        setNewMessage("");
+
+    }
+
+
+
+    return ( 
+        <div className="chat-app">
+            <div className="header">
+                <h1>Welcome to: {room}</h1>
+            </div>
+            <div className="messages-container">
+                <div className="messages">
+                    {messages.map((message) => (
+                        <div className="message" key={message.id}>
+                            <span className="user">
+                                {message.user}:
+                            </span>
+                            {message.text}
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <form onSubmit={handleSubmit} className="new-message-form">
+                <input 
+                    className="new-message-input" 
+                    onChange={(e) => setNewMessage(e.target.value)} 
+                    value={newMessage} 
+                    placeholder="Type a great message here"
+                />
+                <button type="submit" className="send-button">
+                    Send
+                </button>
+            </form>
+        </div>
+    );
+    
+}
